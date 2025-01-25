89c9a6204e6e24d750b5d44a0678535062e86885
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2025-01-25 02:09:48 -0600

    build fixes

--- components/Communities.tsx
+++ components/Communities.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { motion } from 'framer-motion';
+import { motion } from 'motion/react';
 import { MotionWrapper } from '@/components/MotionWrapper';
 import {
   HeartHandshake,
